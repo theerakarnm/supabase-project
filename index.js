36df87dfa9a3d28aedda9d5e8ffbfc940e1722bb
Ryoneme2 <worknarair6@gmail.com>
2022-03-25 22:46:39 +0700

    show all row function

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-import { createClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv'
-dotenv.config();
+const { createClient } = require('@supabase/supabase-js')
+require('dotenv').config()
 
 const supabaseUrl = 'https://dpisjorauglcofczvsxl.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+const showAll = async () => {
+  let { data: db_test, error } = await supabase
+  .from('db_test')
+  .select('*')
+
+console.log(db_test);
+}
+
+showAll();
