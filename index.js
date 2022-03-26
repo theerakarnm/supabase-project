e256d4a2c59044239cda0dac5305fe14c2ca8f2e
Ryoneme2 <worknarair6@gmail.com>
2022-03-26 23:59:23 +0700

    add insert

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const { createClient } = require('@supabase/supabase-js')
-require('dotenv').config()
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
 
-const supabaseUrl = 'https://dpisjorauglcofczvsxl.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl = "https://dpisjorauglcofczvsxl.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const showAll = async () => {
-  let { data: db_test, error } = await supabase
-  .from('db_test')
-  .select('*')
+  let { data: db_test, error } = await supabase.from("db_test").select("*");
 
-console.log(db_test);
-}
+  console.log(db_test);
+};
 
-showAll();
+const insertData = async () => {
+  let i = 10;
+
+  // loop 5 time
+  while (i < 15) {
+    const { data, error } = await supabase.from("db_test").insert([{ id: i }]);
+    console.log(data);
+    i++
+  }
+};
+
+insertData();
